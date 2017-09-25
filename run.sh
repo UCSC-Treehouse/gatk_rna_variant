ded676105124656e37462a17e844a0877cdb39fb
hbeale <hbeale@gmail.com>
2017-09-25 11:54:27 -0700

    run.sh typo

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ freeBayesSettings="--dont-left-align-indels --pooled-continuous --pooled-discret
 
 b=$1
 
-/root/freebayes/bin/freebayes --targets  /ref/TH_precise_merged.bed $freeBayesSettings -f /ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa $b >  ${b/bam}_mini.vcf
+/root/freebayes/bin/freebayes --targets  /ref/TH_precise_merged.bed $freeBayesSettings -f /ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa $b >  ${b/bam}mini.vcf
 
 snpEffSettings="-nodownload -noNextProt -noMotif -noStats -classic -no PROTEIN_PROTEIN_INTERACTION_LOCUS -no PROTEIN_STRUCTURAL_INTERACTION_LOCUS"
 
-java -Xmx10000m -jar /root/snpEff/snpEff.jar -v $snpEffSettings GRCh38.86 ${b/bam}_mini.vcf >  ${b/bam}_mini.ann.vcf
+java -Xmx10000m -jar /root/snpEff/snpEff.jar -v $snpEffSettings GRCh38.86 ${b/bam}mini.vcf >  ${b/bam}mini.ann.vcf
 
-rm ${b/bam}_mini.vcf
+rm ${b/bam}mini.vcf
